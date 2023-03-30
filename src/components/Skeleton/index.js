1beb4b5ae610b39583bb388bfbf86c90730df3c1
Calenzo <calenzodevs@gmail.com>
2023-03-30 03:14:12 -0300

    fix: esling return of component

--- src/components/Skeleton/index.js
+++ src/components/Skeleton/index.js
@@ -2,13 +2,11 @@ import React from "react";
 
 import * as S from "./styles";
 
-export const Skeleton = ({ circle, height, width, isWhite = true }) => {
-  return (
-    <S.Container
-      circle={circle}
-      height={height}
-      width={width}
-      isWhite={isWhite}
-    />
-  );
-};
+export const Skeleton = ({ circle, height, width, isWhite = true }) => (
+  <S.Container
+    circle={circle}
+    height={height}
+    width={width}
+    isWhite={isWhite}
+  />
+);
