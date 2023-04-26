094a6c2f79b91653a677e7af80c8a325e25bf463
Junior331 <jarilson.rk@gmail.com>
2023-04-25 23:54:16 -0300

    Fix

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -5,7 +5,7 @@ import ReactSelect from "react-select";
 import * as S from "./styles";
 export const SelectComponent = ({ label, ...restProps }) => (
   <S.SelectContent>
-    {label && <S.Label>{label}</S.Label>}
+    {label && <S.Label>{label}</S.Label>} 
     <ReactSelect {...restProps} />
   </S.SelectContent>
 );
