6c7bcecc4b3e36d0397f04d21e092b6f09d61b76
Junior331 <jarilson.rk@gmail.com>
2023-04-25 23:50:55 -0300

    Fix: removed code

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -3,9 +3,9 @@ import React from "react";
 import ReactSelect from "react-select";
 
 import * as S from "./styles";
-export const SelectComponent = ({ label, options, ...restProps }) => (
+export const SelectComponent = ({ label, ...restProps }) => (
   <S.SelectContent>
     {label && <S.Label>{label}</S.Label>}
-    <ReactSelect options={options} {...restProps} />
+    <ReactSelect {...restProps} />
   </S.SelectContent>
 );
